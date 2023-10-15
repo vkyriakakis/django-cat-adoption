626662f65c6e47a87b324c5beba330f943abbbf0
vkyriakakis <vkyriakakis@proton.me>
2023-10-15 13:25:54 +0300

    Added new view to display an adopted cat, I should write unist tests next

--- cats/urls.py
+++ cats/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "cats"
 urlpatterns = [
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/adopted", views.AdoptedDetailView.as_view() , name="adopted_detail")
 ]
--- cats/views.py
+++ cats/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
 from django.db.models import Q
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Cat
+from adopt.models import AdoptionRequest
 
 class DetailView(generic.DetailView):
     model = Cat
@@ -11,3 +13,11 @@ class DetailView(generic.DetailView):
 
     def get_queryset(self):
         return Cat.objects.exclude(is_adopted=True).all()
+
+class AdoptedDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Cat
+    template_name = "cats/adopted.html"
+
+    def get_queryset(self):
+        return Cat.objects.filter(adoptionrequest__user=self.request.user,
+                                  adoptionrequest__status=AdoptionRequest.Status.APPROVED)
