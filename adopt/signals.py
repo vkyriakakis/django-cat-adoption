372abc37004e1a6161c3b47e0226e405eeae51c3
vkyriakakis <vkyriakakis@proton.me>
2023-10-24 14:06:36 +0300

    Fixed a bug concerning the reason when a request is REJECTED because another for the same cat was APPROVED

--- adopt/signals.py
+++ adopt/signals.py
@@ -8,9 +8,12 @@ from adopt.models import AdoptionRequest
 def mark_other_adoptions_as_rejected(sender, instance, created, **kwargs):
     # If an adoption request for a cat is approved,
     # all other adoption requests for that cat should
-    # be marked as REJECTED
+    # be marked as REJECTED, and the reason msg to one
+    # that informs the user of the adoption (the reason for previous
+    # REJECTED requests shouldn't be changed)
     if instance.status == AdoptionRequest.Status.APPROVED:
         AdoptionRequest.objects.filter(cat=instance.cat) \
                                .exclude(id=instance.id) \
+                               .exclude(status=AdoptionRequest.Status.REJECTED) \
                                .update(status=AdoptionRequest.Status.REJECTED, \
                                        reason="Someone else adopted the cat first :(")
